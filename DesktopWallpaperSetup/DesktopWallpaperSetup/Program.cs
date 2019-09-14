8a3be3fcdaec2354efd7adee2086093aaf45d162
Jared-Adams <40678919+Jared-Adams@users.noreply.github.com>
2019-09-14 18:01:55 -0400

    Add files via upload

---
+++ DesktopWallpaperSetup/DesktopWallpaperSetup/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DesktopWallpaperSetup
+{
+    class Program
+    {
+        public string StoragePath { get; set; }
+        static void Main(string[] args)
+        {                                                             //  | Sept 14 | Sept 11 | Nov 23
+                                                                      //  V 190914/ 190911/ 191123 - last 2 year, month, day
+            Scraper.DownloadImageNode(@"https://apod.nasa.gov/apod/ap" + DateTime.Now.ToString("yyMMdd") + ".html", //link of today
+                                        "//html/body/center[1]/p[2]/a/@href", //node
+                                        $@"../../Images/Image " +  // storage
+                                        DateTime.Now.ToString("dd-MM-yyyy") + ".png"); //file name coordinated with image date 
+            Thread.Sleep(1000); //prevents image corruption            
+        }
+    }
+}
---
+++ DesktopWallpaperSetup/DesktopWallpaperSetup/Scraper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using HtmlAgilityPack;
+namespace DesktopWallpaperSetup
+{
+    public static class Scraper
+    {
+        internal static string GetDomainNode(string domain, string xPath)
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(domain);
+            var returnString = htmlDoc.DocumentNode.SelectSingleNode(xPath).GetAttributeValue("href", "Node not found.");
+
+            return "https://apod.nasa.gov/apod/" + returnString.ToString();
+        }
+
+        internal static void DownloadImageNode(string domain, string xPath, string storagePath)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFileAsync(new Uri(GetDomainNode(domain, xPath)), storagePath);
+            }
+        }
+    }
+}
